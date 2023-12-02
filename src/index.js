bb1936ce1f61787ae698449bb8eeeb0ee4fbc764
Yash Gandhi <gyash557@gmail.com>
2023-12-02 16:33:42 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-ReactDOM.render(
-   <React.StrictMode>
-     <BrowserRouter>
-       <App />
-     </BrowserRouter>
-   </React.StrictMode>,
-   document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
  );
